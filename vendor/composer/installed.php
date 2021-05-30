b46f2513342e43327a04fd80a982dcd0f5dc3f3c
RudraAugadh <67876352+RudraAugadh@users.noreply.github.com>
2021-05-31 00:47:46 +0530

    Added composer for PHP dependency
    Attempting to get Mysql DB work by adding php through composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6b9ef61d8393bf452768296cbaeb2fd4f0ae3794',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6b9ef61d8393bf452768296cbaeb2fd4f0ae3794',
+    ),
+  ),
+);
